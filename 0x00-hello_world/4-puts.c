9fbb1fa1704834caf510965d042827691585ae8c
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-03 12:28:30 +0100

    random

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlio.h>
+
+main()
+{
+    char str[];
+
+    strcpy(str, "Programming is like building a multilingual puzzle");
+
+    puts(str);
+
+    return(0)
+}
