c3b5f1afd8b3b11a7d9504c0039a55cf957b41c3
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-03 13:26:01 +0100

    random

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
-int main () {
-
-    char str[52] = "Programming is like building a multilingual puzzle";
-
-    strcpy(str);
-
-    puts(str);
-
-    return(0);
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
