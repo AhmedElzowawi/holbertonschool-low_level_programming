28bca4c02103eeb75c51619418a17e6caf28f336
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-08 17:25:37 +0100

    random

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_to_98 - prints if number is positive, 0 or negative
+  * @n: charecter
+  * Return: 0
+  */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+	return (n);
+}
