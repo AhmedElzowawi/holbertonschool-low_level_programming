1f9d8bb242a42a1474fcacbe9a379f4c0367a725
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-08 19:08:38 +0100

    random

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,91 @@
+#include "main.h"
+
+/**
+  * times_table - prints if number is positive, 0 or negative
+  * Return: 0
+  */
+void times_table(void)
+{
+int a, b, multiplay;
+
+for (b = 0; b < 10; b++)
+{
+	a = 0;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+for (b = 0; b < 10; b++)
+{
+	a = 1;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+for (b = 0; b < 10; b++)
+{
+	a = 2;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+for (b = 0; b < 10; b++)
+{
+	a = 3;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+for (b = 0; b < 10; b++)
+{
+	a = 4;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+for (b = 0; b < 10; b++)
+{
+	a = 5;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+for (b = 0; b < 10; b++)
+{
+	a = 6;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+for (b = 0; b < 10; b++)
+{
+	a = 7;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+for (b = 0; b < 10; b++)
+{
+	a = 8;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+for (b = 0; b < 10; b++)
+{
+	a = 9;
+	multiplay = a * b;
+	_putchar(multiplay);
+	_putchar(',');
+	_putchar(' ');
+}
+}
