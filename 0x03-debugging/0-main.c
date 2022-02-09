0964eea569c2f3e9a2867230c14a9399c39b9333
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-09 17:09:14 +0100

    random

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
-  * main - prints if number is positive, 0 or negative
-  * Return: 0
-  */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	 int i = 0;
+	int i;
 
-	postitive_or_negative(0);
-	return (i);
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
