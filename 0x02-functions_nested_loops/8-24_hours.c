0c2a336b82bbaf33bd531092498a7d7a26cfd54d
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-09 12:40:48 +0100

    random

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,9 +11,11 @@ void jack_bauer(void)
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
-		_putchar(h + '0');
+		_putchar((h / 10 ) + '0');
+		_putchar((h % 10 ) + '0');
 		_putchar(':');
-		_putchar(m + '0')
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
 		_putchar('\n');
 	}
 	return (0);
---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * main - prints if number is positive, 0 or negative
+  * Return: 0
+  */
+int main(void)
+{
+	int i = 10;
+
+	postitive_or_negative(i);
+	return (0);
+}
