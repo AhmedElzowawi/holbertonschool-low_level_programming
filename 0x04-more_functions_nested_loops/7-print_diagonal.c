58b890393d1f6df92a2f88f6ebbbf58b3c6a9006
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-10 12:38:38 +0100

    random

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ *@n: parameter
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+		for (b = 0; b < x; b++)
+		{
+		_putchar(' ');
+		}
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - check the code
+ *@size: parameter
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int i;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+	_putchar(35);
+	_putchar('\n');
+	}
+	}
+}
