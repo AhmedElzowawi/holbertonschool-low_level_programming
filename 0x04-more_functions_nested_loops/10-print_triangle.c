4fd7afda23b32d5f909448a2ffec7563538c922d
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-10 21:57:26 +0100

    random

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - check the code
+ * print_triangle - check the code
+ *@size: parameter
  * Return: Always 0.
  */
 void print_triangle(int size)
@@ -18,7 +19,7 @@ void print_triangle(int size)
 	for (a = 0; a < size; a++)
 	{
 		for (b = size - a; b > 1; b--)
-		{	
+		{
 		_putchar(' ');
 		}
 		for (c = 0; c <= a; c++)
