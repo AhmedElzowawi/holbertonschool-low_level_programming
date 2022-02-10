83f72281de0b486d353111284645ab51d56ff063
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-10 12:51:21 +0100

    random

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,23 +5,23 @@
  * main - check the code
  * Return: Always 0.
  */
-void main(void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-	if ((i % 3 == 0) && (i % 5 == 0))
+	if (i % 3 == 0)
 	{
-		printf("FizzBuzz");
-	}
-	else if (i % 3 == 0)
-	{
-		printf("Fizz");
+	printf("Fizz");
 	}
 	else if (i % 5 == 0)
 	{
-	printf("Buzz");
+		printf("Buzz");
+	}
+	else if ((i % 3 == 0) && (i % 5 == 0))
+	{
+		printf("FizzBuzz");
 	}
 	else
 	{
