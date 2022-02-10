2a161f308ee5db47517215cce202d84af6bdfaae
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-10 21:55:01 +0100

    random

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,7 +9,7 @@ void print_triangle(int size)
 {
 	int a, b, c;
 
-	if (size < 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
@@ -17,7 +17,7 @@ void print_triangle(int size)
 	{
 	for (a = 0; a < size; a++)
 	{
-		for (b = 1; b < size; b++)
+		for (b = size - a; b > 1; b--)
 		{	
 		_putchar(' ');
 		}
