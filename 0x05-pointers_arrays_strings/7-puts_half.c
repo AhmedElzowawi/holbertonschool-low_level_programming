54fafdff5b0dde42d05843fe6cbc0ede0b21865b
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-14 13:18:05 +0100

    random

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ * @str: parameter
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n;
+
+	while (str[i] != '\0')
+	{
+	i++;
+	}
+	if (i % 2 == 0)
+	{
+	n = i / 2;
+	}
+	else
+	{
+	n = (i - 1) / 2;
+	}
+	while (n < i)
+	{
+	_putchar(str[n]);
+	n++;
+	}
+	_putchar('\n');
+}
