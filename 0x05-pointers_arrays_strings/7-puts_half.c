9e9d24ee45185c00f723a21de1e4af68d049b5c5
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-14 18:29:05 +0100

    random

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,6 +15,10 @@ void puts_half(char *str)
 	length++;
 	}
 	if (length % 2 == 0)
+	for (secondLength = (length / 2); secondLength <= (length - 1) secondLength++)
+	{
+		_putchar(str[secondLength]);
+	}
 	{
 	secondLength = length / 2;
 	}
@@ -22,7 +26,7 @@ void puts_half(char *str)
 	{
 	secondLength = (length + 1) / 2;
 	}
-	while (secondLength <= length)
+	while (secondLength <= (length -1))
 	{
 	_putchar(str[secondLength]);
 	secondLength++;
