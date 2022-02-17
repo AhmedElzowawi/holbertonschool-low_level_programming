9e76443341c3946e15da3f13425b0f3d1dd79e09
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-17 10:58:45 +0100

    random

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,4 +6,17 @@
  *@s: parameter
  * Return: Always 0.
  */
- 
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+	s[i] -= 32;
+	}
+	i++;
+}
+}
