b140ab164ac78d95fcb4f011f62e6473b8ce3130
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-17 11:01:50 +0100

    random

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,11 +13,11 @@ int j;
 char l[] = "aAeEoOtTlL";
 char n[] = "43071";
 
-for (i = 0 ;s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; l[j] != '\0'; j++)
 {
-if (s[i] == l[j] )
+if (s[i] == l[j])
 {
 s[i] = n[j / 2];
 }
