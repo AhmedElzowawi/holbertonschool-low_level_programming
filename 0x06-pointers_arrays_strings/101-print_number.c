16dd2140bdbec06627d14906077ff584d0bae7b7
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-18 10:48:55 +0100

    random

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,12 +5,13 @@
  * @n: parameter
  * Return: Always 0.
  */
-void pr_int(int n) {
+void print_number(int n)
+{
     if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10 != 0)
-        pr_int(n / 10);
-    _putchar((n % 10) + '0');
+	_putchar('-');
+	n = -n;
+	}
+	if (n / 10 != 0)
+	print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
