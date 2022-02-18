c5ace95468af33c6261c54357be5f2a64df5bbf1
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-18 09:14:27 +0100

    random

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,23 +8,20 @@
 
 char *rot13(char *s)
 {
-	int i;
-	int j;
-	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char n[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char l1[] = "abcdefghijklmnopqrstuvwxyz";
-	char n2[] = "nopqrstuvwxyzabcdefghijklm";
+int i;
+int j;
+char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char n[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	for (j = 0; l[j] != '\0' && l1[j] != '\0'; j++)
-	{
-	if (s[i] == l[j] || s[i] == l1[j])
-	{
-	s[i] = n[j];
-	s[i] = n2[j];
-	}
-	}
-	}
-	return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; l[j] != '\0'; j++)
+{
+if (s[i] == l[j])
+{
+s[i] = n[j];
+}
+}
+}
+return (s);
 }
