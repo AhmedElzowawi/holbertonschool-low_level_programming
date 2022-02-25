a29b2542a2df7586b90dd13329105245472628ba
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-02-25 21:49:27 +0100

    ranodm

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - check the code
+ * @argc: parameter
+ * @argv: parameter
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j; /* minimum number of coins */
+	int money = atoi(argv[1]);
+	int values[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	if (money < 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 0; money != 0; i++)
+		{	
+			j += money / values[i];
+			money %= values[i];		
+		}
+		printf("%d\n", j);
+	}
+	return (0);
+}
