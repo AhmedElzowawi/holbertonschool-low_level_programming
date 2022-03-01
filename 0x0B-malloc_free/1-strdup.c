7f33194552f503188ae9b7fc6e88f32945272681
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-01 11:57:00 +0100

    random

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,15 +10,15 @@ char *_strdup(char *str)
 	char *ptr;
 	unsigned int i;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
 	ptr = (char *) malloc(sizeof(char) * i + 1);
 	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-	else if (str == NULL)
 	{
 		return (NULL);
 	}
