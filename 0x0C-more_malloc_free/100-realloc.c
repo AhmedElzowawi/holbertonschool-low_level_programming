bd956e6488e46506e3a5da76d838d6edc3a9a83a
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-03 18:53:34 +0100

    random

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,11 +34,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 	if (new_size > old_size)
 	{
+		if (ptrNew)
+		{
 			for (i = 0; i < old_size; i++)
 			{
 				ptrNew[i] = ptrOld[i];
 			}
 		free(ptr);
+		}
 	}
 	return (ptrNew);
 }
