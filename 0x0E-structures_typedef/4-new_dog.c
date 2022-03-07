a118da9098146350d6c03ecf69a3ad487f773af6
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-07 11:36:05 +0100

    random

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - check the code
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+	return (ptr);
+}
