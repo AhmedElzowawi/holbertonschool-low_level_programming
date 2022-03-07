ce066db434f1fbed70cb566a2eb1dbd3bd6d748d
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-07 15:18:22 +0100

    random

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,63 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 /**
- * new_dog - check the code
- * @name: parameter
- * @age: parameter
- * @owner: parameter
- * Return: Always 0.
+ * new_dog - creates new doggy
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of dog
+ * Return: struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, j, k, l;
 	dog_t *dog;
 
-	for (i = 0; name[i]; i++)
-	{
-	}
-	for (j = 0; owner[j]; j++)
-	{
-	}
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * i);
-	if (dog->name == NULL)
+	dog->name = malloc(_strlen(name) + 1);
+	dog->owner = malloc(_strlen(owner) + 1);
+	if (dog->name == NULL || dog->owner == NULL)
 	{
-		free(dog);
-		return (NULL);
-	}
-	for (k = 0; k < i; k++)
-	{
-		dog->name[k] = name[k];
-	}
-	dog->age = age;
-	dog->owner = malloc(sizeof(char) * j);
-	if (dog->owner == NULL)
-	{
+		free(dog->owner);
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	for (l = 0; l < j; l++)
-	{
-		dog->owner[l] = owner[l];
-	}
+	dog->name = _strcpy(dog->name, name);
+	dog->owner = _strcpy(dog->owner, owner);
+	dog->age = age;
 	return (dog);
+}
+/**
+ * _strlen - counts string length of s
+ * @s: parameter
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * _strcpy - copies string
+ * @dest: destination
+ * @src: destination
+ * Return: (dest value)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *saved = dest;
+
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = 0;
+	return (saved);
 }
