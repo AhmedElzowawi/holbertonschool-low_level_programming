d49267c54e15a11ad701fa26acfb679cddf1bb40
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-07 14:13:43 +0100

    random

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,12 +16,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
+	dog->name = malloc(sizeof(name));
 	if (dog->name == NULL)
 	{
 		free(dog->owner);
 		free(dog);
 		return (NULL);
 	}
+
+	dog->owner = malloc(sizeof(owner));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
