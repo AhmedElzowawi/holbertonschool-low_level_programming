e2517e2f6735400949f9af0cbf7554efe8c22ad8
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-09 23:04:04 +0100

    random

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -22,8 +22,14 @@ int main(int argc, char **argv)
 		exit(2);
 	}
 	ptr = (char *)main;
-	for (i = 0; i < atoi(argv[1]) - 1; i++)
+	for (i = 0; i < atoi(argv[1]); i++)
+	{
+		if (i == atoi(argv[1]) - 1)
+		{
+			printf("%02hhx\n", ptr[i]);
+			break;
+		}
 		printf("%02hhx", ptr[i]);
-	printf("%02hhx\n", ptr[i]);
+	}
 	return (0);
 }
