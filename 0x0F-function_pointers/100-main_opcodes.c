344e281e10bc765e239a2fc16fc0c6a5c8246b9f
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-09 23:14:52 +0100

    random

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,31 +8,22 @@
  */
 int main(int argc, char **argv)
 {
-	int i, bytes;
 	char *ptr;
+	int i;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
+	if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 	ptr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
-		{
-			if (i == bytes - 1)
-			{
-				printf("%02hhx\n", ptr[i]);
-				break;
-			}
-			printf("%02hhx", ptr[i]);
-		}
+	for (i = 0; i < atoi(argv[1]) - 1; i++)
+		printf("%02hhx ", ptr[i]);
+	printf("%02hhx", ptr[i]);
 	return (0);
 }
