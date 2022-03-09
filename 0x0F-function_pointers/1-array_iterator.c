bfdfce348c70e4356da00e89bff7d70b8a39b487
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-09 11:58:29 +0100

    random

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints a name as is
+ * @array: name of the person
+ * @size: function
+ * @action: funcion
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
