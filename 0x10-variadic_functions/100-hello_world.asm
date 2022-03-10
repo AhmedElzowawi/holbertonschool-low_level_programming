1258c99eb0b0682ab859b2eee8837440e2aa568f
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-10 13:35:11 +0100

    random

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,17 +1,27 @@
-section .rodata
-  msg: db "Hello, World", 10
-  msglen: equ $ - msg
 section .text
-global _start
+; Export the entry point to the ELF linker or loader.  The conventional
+; entry point is "_start". Use "ld -e foo" to override the default.
+    global _start
 
+section .data
+msg db  'Hello, World',0xa ;our dear string
+len equ $ - msg         ;length of our dear string
+
+section .text
+
+; linker puts the entry point here:
 _start:
-  mov rax, 1    
-  mov rdi, 1  
-  mov rsi, msg  
-  mov rdx, msglen
-  syscall    
 
-  mov rax, 60 
-  mov rdi, 0 
-  syscall
-  end:	
+; Write the string to stdout:
+
+    mov edx,len ;message length
+    mov ecx,msg ;message to write
+    mov ebx,1   ;file descriptor (stdout)
+    mov eax,4   ;system call number (sys_write)
+    int 0x80    ;call kernel
+
+; Exit via the kernel:
+
+    mov ebx,0   ;process' exit code
+    mov eax,1   ;system call number (sys_exit)
+    int 0x80    ;call kernel - this interrupt won't return
