1780dc3a03b98373bbe274e8b61b725a38091ff1
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-10 12:08:25 +0100

    random

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,5 +1,5 @@
 section .data
-    msg db "Hello, world!",10      ; 10 is the ASCII code for a new line (LF)
+    msg db "Hello, World",10      ; 10 is the ASCII code for a new line (LF)
 
 section .text
     global _start
@@ -8,7 +8,7 @@ _start:
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
-    mov rdx, 14
+    mov rdx, 13
     syscall
     
     mov rax, 60
