25a4bf1c9e3946c35085ad0311e4057879fe6d94
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-10 12:52:31 +0100

    random

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,18 +1,16 @@
-global _start
-
-section .text
-
-_start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, world!\n",
-  mov rdx, msglen   ;   sizeof("Hello, world!\n")
-  syscall           ; );
-
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
-
 section .rodata
   msg: db "Hello, World", 10
   msglen: equ $ - msg
+section .text
+global _start
+
+_start:
+  mov rax, 1    
+  mov rdi, 1  
+  mov rsi, msg  
+  mov rdx, msglen
+  syscall    
+
+  mov rax, 60 
+  mov rdi, 0 
+  syscall
