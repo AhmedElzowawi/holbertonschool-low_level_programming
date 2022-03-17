7944b4f0f3dd787227d0043795f2c23cab50241e
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-17 18:56:52 +0100

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
