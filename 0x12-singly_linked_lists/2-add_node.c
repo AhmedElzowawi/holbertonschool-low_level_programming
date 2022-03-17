a67625229436943abaa8ade02db3a15e59390d12
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-17 23:28:43 +0100

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - check the code
+ * @h: parameter
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp;
+	int i = 0;
+
+	tmp = malloc(sizeof(list_t));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->str = strdup(str);
+	tmp->next = NULL;
+
+	while (str[i])
+		i++;
+	tmp->len = i;
+
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
+}
