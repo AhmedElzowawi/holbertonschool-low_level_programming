5f87e7aa85e347ddee38dc91e22cfe5f1779fc2b
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-22 21:30:39 +0100

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,23 +10,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *current = *head;
-	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *new;
 
-	if (!new)
-		return (NULL);
-	if (!*head)
-		return (NULL);
 	for (i = 0; current; i++)
 	{
 		if (i == idx - 1)
 		{
-			new->n = n;
-			new->next = current->next;
-			current->next = new;
-			return (new);
+			new = malloc(sizeof(listint_t));
+			if (!new)
+				return (NULL);
+			else
+			{
+				new->n = n;
+				new->next = current->next;
+				current->next = new;
+				return (new);
+			}
 		}
-		if (current->next)
-			current = current->next;
+		current = current->next;
 	}
 	return (NULL);
 }
