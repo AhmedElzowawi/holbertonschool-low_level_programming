cc771f3ccf24128fd87aa39b62f7e2c724eb6365
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-22 18:08:13 +0100

    fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (head == NULL)
+		return;
 	while (*head)
 	{
 		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
 	}
-	head = 0;
+	*head = NULL;
 }
