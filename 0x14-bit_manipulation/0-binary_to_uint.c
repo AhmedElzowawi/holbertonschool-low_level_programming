8929ab5e1ba36fbe2ce9140966c824614046e7c9
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-24 19:57:39 +0100

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * binary_to_unit - check the code
+ * binary_to_uint - check the code
  * @b: parameter
  * Return: Always 0.
  */
@@ -9,6 +9,8 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0, decval = 1;
 	unsigned int sum = 0;
 
+	if (!b)
+		return (0);
 	while (b[i])
 		i++;
 	for (i -= 1; i >= 0; i--)
