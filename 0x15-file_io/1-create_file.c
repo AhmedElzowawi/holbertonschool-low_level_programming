32c5472cba80e6f46f6cbf37dceae8f6aaced105
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-28 21:48:16 +0100

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,8 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i])
 		i++;
 	fail = write(file, text_content, i);
+	if (!fail)
+		return (0);
 	close(file);
 		return (1);
 }
