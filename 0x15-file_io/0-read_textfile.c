00d89d97206f2b376f368e48086a056a01606443
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-28 18:08:33 +0100

    test

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,20 +7,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, readFile;
+	int file;
 	char *buf;
 
 	if (!filename)
 		return (0);
-	buf = malloc(sizeof(letters));
+	buf = malloc(letters);
 	if (!buf)
 		return (0);
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
-	readFile = read(file, buf, letters);
-	write(STDOUT_FILENO, buf, readFile);
+	read(file, buf, letters);
+	write(STDOUT_FILENO, buf, letters);
 	close(file);
 	free(buf);
-		return (readFile);
+		return (letters);
 }
