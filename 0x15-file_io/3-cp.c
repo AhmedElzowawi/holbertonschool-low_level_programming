cf420e9c14c31386343017bc854d8b17987aa23f
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-29 20:41:11 +0100

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,22 +1,5 @@
 #include "main.h"
 /**
- * main - check the code
- * @argc: parameter
- * @argv: parameter
- * Return: Always 0.
- */
- void cp(const char *from, const char *to);
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	cp(argv[1], argv[2]);
-	return (0);
-}
-/**
  * cp - check the code
  * @from: parameter
  * @to: parameter
@@ -33,7 +16,6 @@ void cp(const char *from, const char *to)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
 		exit(98);
 	}
-	size = read(fileR, buf, 1024);
 
 	fileW = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fileW == -1 || to == NULL)
@@ -41,6 +23,7 @@ void cp(const char *from, const char *to)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
 		exit(99);
 	}
+	size = read(fileR, buf, 1024);
 	write(fileW, buf, size);
 
 	if (close(fileR) == -1)
@@ -54,4 +37,20 @@ void cp(const char *from, const char *to)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileW);
 		exit(100);
 	}
+}
+/**
+ * main - check the code
+ * @argc: parameter
+ * @argv: parameter
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	cp(argv[1], argv[2]);
+	return (0);
 }
