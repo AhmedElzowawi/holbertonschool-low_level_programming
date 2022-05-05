a778f1ddaca9feb01111529615d4c8c9ad6e0c12
ahmed4403 <4403@holbertonschool.com>
2022-05-05 16:47:01 +0000

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *new, *ptr;
+	dlistint_t *new, *ptr, *tmp;
 
 	if (h == NULL)
 		return (NULL);
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	ptr = *h;
-	for (i = 0; ptr->next; ptr = ptr->next, i++)
+	for (i = 0; ptr; ptr = ptr->next, i++)
 	{
 		if (i == idx)
 		{
@@ -35,10 +35,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			ptr->prev = new;
 			return (new);
 		}
+		if (ptr->next == NULL)
+			tmp = ptr;
 	}
 	new->n = n;
-	ptr->next = new;
 	new->prev = ptr;
+	tmp->next = new;
 	new->next = NULL;
 	return (new);
 }
