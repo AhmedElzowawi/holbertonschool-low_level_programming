d565efe8da32c8c751aa7d660f4e31a93ec5b6a5
ahmed4403 <4403@holbertonschool.com>
2022-05-05 11:58:03 +0000

    test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new == NULL)
 		return (NULL);
-	if (h == NULL)
+	if (h == NULL || *h == NULL)
 		return (NULL);
 	new->n = n;
 	if (idx > 0)
