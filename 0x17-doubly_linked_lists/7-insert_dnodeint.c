4174e5cbe10764f47084389f987e909a7b979c63
ahmed4403 <4403@holbertonschool.com>
2022-05-05 12:19:53 +0000

    test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *new, *ptr;
 
-	if (h)
+	if (h && *h)
 	{
 		new = malloc(sizeof(dlistint_t));
 		if (new == NULL)
