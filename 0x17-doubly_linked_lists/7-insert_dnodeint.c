4ff6b637c14a510648fc1f829096d0ffbf858205
ahmed4403 <4403@holbertonschool.com>
2022-05-05 16:54:11 +0000

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,7 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			tmp = ptr;
 	}
 	new->n = n;
-	new->prev = ptr;
+	new->prev = tmp;
 	tmp->next = new;
 	new->next = NULL;
 	return (new);
